5c36ab776e79576ea7315a52b82aa1d39836170f
Jordan-Watson <jordanwatson000@gmail.com>
2023-12-12 19:23:12 +0000

    added color to vote button for visual acknowledgement that the request is happening

--- src/tools-buttons/VotingButton.jsx
+++ src/tools-buttons/VotingButton.jsx
@@ -7,22 +7,29 @@ import { useState } from 'react'
 const VotingButton = ({ id, votes }) => {
   const [currVotes, setCurrVotes] = useState(votes)
   const [timesClicked, setTimesClicked] = useState(0)
-
+  const [buttonColor1, setButtonColor1] = useState('default')
+  const [buttonColor2, setButtonColor2] = useState('default')
 
   const upVote = () => {
     if (timesClicked <= 0) {
-
+      setButtonColor1('secondary')
       setTimesClicked(timesClicked + 1)
       setCurrVotes(currVotes + 1)
-      addVotesToArticleId(1, id)
+      addVotesToArticleId(1, id).then(() => {
+        setTimeout(() => { setButtonColor1('default') }, 100)
+
+      })
     }
     else { }
   }
   const downVote = () => {
     if (timesClicked >= 0) {
+      setButtonColor2('secondary')
       setTimesClicked(timesClicked - 1)
       setCurrVotes(currVotes - 1)
-      addVotesToArticleId(-1, id)
+      addVotesToArticleId(-1, id).then(() => {
+        setTimeout(() => { setButtonColor2('default') }, 100)
+      })
     }
 
   }
@@ -30,14 +37,14 @@ const VotingButton = ({ id, votes }) => {
 
   return (
     <>
-      <Box className="thumbs-up">
+      <Box className="thumbs-up" >
         <p style={{ display: 'inline', marginRight: '20px' }}>Votes: {currVotes}</p>
-        <Fab size="small" onClick={upVote}>
+        <Fab size="small" onClick={upVote} color={buttonColor1}>
           <ThumbUpIcon />
         </Fab>
       </Box>
-      <Box className="thumbs-down">
-        <Fab size="small" onClick={downVote}>
+      <Box className="thumbs-down" >
+        <Fab size="small" color={buttonColor2} onClick={downVote}>
           <ThumbDownIcon />
         </Fab>
       </Box>
