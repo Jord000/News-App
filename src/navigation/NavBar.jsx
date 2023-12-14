3f5ceb718257d83f1a55b69de221dea2e48490a4
Jordan-Watson <jordanwatson000@gmail.com>
2023-12-14 11:13:33 +0000

    added tag to see what topic you are looking at

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -9,13 +9,18 @@ const NavBar = () => {
   const [sort, setSort] = useState(searchParams.get('sort') || '')
   const [order, setOrder] = useState(searchParams.get('order') || 'ASC')
 
+  let topic = searchParams.get('topic')
+
   return (
-    <div className='navBar'>
-      <MenuNav />
-      <TopicNav setOrder={setOrder} setSort={setSort} />
-      {location.pathname === '/myfeed' &&
-        <SortArticle order={order} sort={sort} setOrder={setOrder} setSort={setSort} />}
-    </div>
+    <>
+      <div className='navBar'>
+        <MenuNav />
+        <TopicNav setOrder={setOrder} setSort={setSort} />
+        {location.pathname === '/myfeed' &&
+          <SortArticle order={order} sort={sort} setOrder={setOrder} setSort={setSort} />}
+      </div>
+      {topic && <p className='topic-tag'>{topic.charAt(0).toUpperCase() + topic.slice(1)}</p>}
+    </>
   )
 }
 
