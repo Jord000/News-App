68d6d0f2cdb133222887af9f646fbd3f1b1f4ec3
Jordan <jordanwatson000@gmail.com>
2023-12-15 15:45:19 +0000

    update new build info

--- src/main-feed/OneArticle.jsx
+++ src/main-feed/OneArticle.jsx
@@ -1,100 +1,141 @@
-import { Grid, Fab, Box, ButtonBase, createTheme, ThemeProvider, duration } from "@mui/material"
-import { truncateText } from "../../utils/truncateText"
-import MoreIcon from '@mui/icons-material/More';
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-
+import {
+  Grid,
+  Fab,
+  Box,
+  ButtonBase,
+  createTheme,
+  ThemeProvider,
+  duration,
+} from '@mui/material'
+import { truncateText } from '../../utils/truncateText'
+import MoreIcon from '@mui/icons-material/More'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 const OneArticle = ({ article }) => {
-    const { author, comment_count, created_at, title, votes, topic, body, article_id, article_img_url } = article
-    const navigate = useNavigate()
-    const [screen, setScreen] = useState(window.innerWidth)
-    const breakpointValues = {
-        xs: 0,
-        sm: 500,
-        md: 1000,
-        lg: 1200,
-        xl: 1500,
-    };
-    const theme = createTheme({ breakpoints: { values: breakpointValues } });
+  const {
+    author,
+    comment_count,
+    created_at,
+    title,
+    votes,
+    topic,
+    body,
+    article_id,
+    article_img_url,
+  } = article
+  const navigate = useNavigate()
+  const [screen, setScreen] = useState(window.innerWidth)
+  const breakpointValues = {
+    xs: 0,
+    sm: 500,
+    md: 1000,
+    lg: 1200,
+    xl: 1500,
+  }
+  const theme = createTheme({ breakpoints: { values: breakpointValues } })
 
+  const ConditionalScreenDiv = ({ style, className, size, children }) => {
+    return screen > size ? (
+      <div style={style} className={className}>
+        {children}
+      </div>
+    ) : (
+      <>{children}</>
+    )
+  }
 
-    const ConditionalScreenDiv = ({ style, className, size, children }) => {
-        return screen > size ? <div style={style} className={className}>{children}</div> :
-            <>{children}</>
+  const handleResize = () => {
+    setScreen(window.innerWidth)
+    if (screen > 1000) {
+      articleDisplay = 'flex'
     }
+  }
 
-    const handleResize = () => {
-        setScreen(window.innerWidth)
-        if (screen > 1000) { articleDisplay = 'flex' }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
     }
+  }, [])
 
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [])
-
-
-
-    return <>
-        <ThemeProvider theme={theme}>
-            <Grid item xs={7.5} onClick={() => { navigate(`/myfeed/${article_id}`) }}
-                style={{
-                    backgroundColor: 'rgb(249, 245, 250)',
-                    padding: '10px',
-                    borderRadius: '8px',
-                    filter: 'drop-shadow(2px 2px 4px grey)',
-                    marginBottom: '30px',
-
-
-                }}
-                sx={{
-                    display: { xs: "block", md: 'flex' },
-                    transition: theme.transitions.create("background-color 100s, box-shadow 100s"),
-                    "&:hover": {
-                        transition: '.5s ease',
-                        backgroundColor: '#cab5cc',
-                        boxShadow: '4px 4px 8px #666'
-                    },
-                    '&.active': {
-                        transform: translateY('4px'),
-                        backgroundColor: 'black',
-                    },
-
-                }}
-            >   <ConditionalScreenDiv size={1000} className={"main-feed-conditional-wrapper"}>
-                    <div className="main-feed-title-img-container">
-                        <div style={{ width: '100%' }}>
-                            <h2 className="main-feed-article-title">{title}</h2>
-                            <p className="article-detail">Topic - {topic}</p>
-                        </div>
-                        {screen < 1000 && <div className="main-feed-article-icon-container"><img
-                            className="main-feed-article-icon"
-                            src={article_img_url}
-                            alt={`image relating to ${title}`}
-                        /></div>}
-                    </div>
-                    <p className="article-detail">Posted by author - {author}</p>
-                    <p className="article-detail">Date Posted- {created_at.substring(0, 10)}</p>
-                    <p className="small-article-body">{truncateText(body)}</p>
-                    <div className="read-more-container">
-                        <Link to={`/myfeed/${article_id}`}>
-                            <Fab sx={{ backgroundColor: '#6e94db', ml: 'auto' }} variant="extended" size="small" color="primary" >
-                                <MoreIcon className="more-icon" />
-                            </Fab>
-                        </Link>
-                    </div>
-                </ConditionalScreenDiv>
-                {screen > 1000 && <img
-                    className="main-feed-article-img"
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Grid
+          item
+          xs={7.5}
+          onClick={() => {
+            navigate(`/myfeed/${article_id}`)
+          }}
+          style={{
+            backgroundColor: 'rgb(249, 245, 250)',
+            padding: '10px',
+            borderRadius: '8px',
+            filter: 'drop-shadow(2px 2px 4px grey)',
+            marginBottom: '30px',
+          }}
+          sx={{
+            display: { xs: 'block', md: 'flex' },
+            transition: theme.transitions.create(
+              'background-color 100s, box-shadow 100s'
+            ),
+            '&:hover': {
+              transition: '.5s ease',
+              backgroundColor: '#cab5cc',
+              boxShadow: '4px 4px 8px #666',
+            },
+          }}
+        >
+          {' '}
+          <ConditionalScreenDiv
+            size={1000}
+            className={'main-feed-conditional-wrapper'}
+          >
+            <div className="main-feed-title-img-container">
+              <div style={{ width: '100%' }}>
+                <h2 className="main-feed-article-title">{title}</h2>
+                <p className="article-detail">Topic - {topic}</p>
+              </div>
+              {screen < 1000 && (
+                <div className="main-feed-article-icon-container">
+                  <img
+                    className="main-feed-article-icon"
                     src={article_img_url}
                     alt={`image relating to ${title}`}
-                />}
-            </Grid >
-        </ThemeProvider >
+                  />
+                </div>
+              )}
+            </div>
+            <p className="article-detail">Posted by author - {author}</p>
+            <p className="article-detail">
+              Date Posted- {created_at.substring(0, 10)}
+            </p>
+            <p className="small-article-body">{truncateText(body)}</p>
+            <div className="read-more-container">
+              <Link to={`/myfeed/${article_id}`}>
+                <Fab
+                  sx={{ backgroundColor: '#6e94db', ml: 'auto' }}
+                  variant="extended"
+                  size="small"
+                  color="primary"
+                >
+                  <MoreIcon className="more-icon" />
+                </Fab>
+              </Link>
+            </div>
+          </ConditionalScreenDiv>
+          {screen > 1000 && (
+            <img
+              className="main-feed-article-img"
+              src={article_img_url}
+              alt={`image relating to ${title}`}
+            />
+          )}
+        </Grid>
+      </ThemeProvider>
     </>
+  )
 }
 
 export default OneArticle
